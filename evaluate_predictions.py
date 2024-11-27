708e23818ad07c4b15eed24fe5a624787d7f7f41
bbrie <b.bries@hotmail.com>
2024-11-28 00:01:48 +0100

    Cleaned up visualize files, added classifier arg to evaluate predictions to match new folder structure

--- evaluate_predictions.py
+++ evaluate_predictions.py
@@ -25,14 +25,15 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description='Compute Accuracy, Precision, Recall, F1-score')
 
     argparser.add_argument('-d', '--dataset', default='NSL-KDD', type=str, help='The datset to split as a string')
+    argparser.add_argument('--classifier', default="ebmclassifier", type=str, help='The classifier as a string.')
 
     args = argparser.parse_args()
 
     if args.dataset is None:
         raise ValueError(f"Dataset argument not provided. Use the -d or --dataset flag to provide one of {valid_datasets}.")
 
-    print("The evaluated dataset is: ", args.dataset)
-    pred_vs_true_path = f"results/{args.dataset}/pred_vs_true.csv"
+    print(f"The evaluated dataset is {args.dataset} using classifier {args.classifier}.")
+    pred_vs_true_path = f"results/{args.dataset}/pred_vs_true_{args.classifier}.csv"
 
     if not os.path.exists(pred_vs_true_path):
         raise FileNotFoundError(f"File not found: {pred_vs_true_path}")
